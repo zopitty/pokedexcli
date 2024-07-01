3b00ce1ad97990a62bb3aca87dd3da6391b0b056
zopitty <maxtan96@gmail.com>
2024-07-02 01:42:11 +0800

    create location area type for api json

--- commands/help.go
+++ commands/help.go
@@ -3,13 +3,14 @@ package commands
 import "fmt"
 
 func commandHelp() error {
-    fmt.Println("===============")
+    fmt.Println("=======================")
     fmt.Println("Welcome to the pokedex!")
     fmt.Println("Usage:")
-    fmt.Println("===============")
+    fmt.Println()
     for _, cmd := range GetCommands() {
         fmt.Printf("%s: %s\n", cmd.Name, cmd.Description)
     }
     fmt.Println()
+    fmt.Println("=======================")
     return nil
 }
