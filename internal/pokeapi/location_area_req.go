25af15fe484e9bf36bea7f2d79a1591021636f80
zopitty <maxtan96@gmail.com>
2024-07-02 01:42:52 +0800

    create http client for api call and api call

---
+++ internal/pokeapi/location_area_req.go
@@ -0,0 +1,42 @@
+package pokeapi
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func (c *Client) ListLocationAreas() (ResLocationArea, error) {
+    endpoint := "/location-area"
+    fullURL := baseURL + endpoint
+
+    req, err := http.NewRequest("GET", fullURL, nil)
+    if err != nil {
+        return ResLocationArea{}, err
+    }
+
+    res, err := c.httpClient.Do(req)
+    if err != nil {
+        return ResLocationArea{}, err
+    }
+    defer res.Body.Close()
+
+    if res.StatusCode > 399 {
+        return ResLocationArea{}, fmt.Errorf("bad status code: %v", res.StatusCode)
+    }
+
+    dat, err := io.ReadAll(res.Body)
+    if err != nil {
+        return ResLocationArea{}, err
+    }
+
+    resLocationArea := ResLocationArea{}
+    err = json.Unmarshal(dat, &resLocationArea)
+    if err != nil {
+        return ResLocationArea{}, err
+    }
+
+    return resLocationArea, nil
+}
+
