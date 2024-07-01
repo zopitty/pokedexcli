7ed9b61086a16118f637a7a153ec92af9127c7c0
zopitty <maxtan96@gmail.com>
2024-07-01 16:12:12 +0800

    update main.go

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
     "fmt"
-    "github.com/zopitty/pokedexcli"
+    "github.com/zopitty/pokedexcli/repl"
 )
 
 func main() {
+    fmt.Println("POKEDEX!!")
+    repl.StartREPL()
 }
